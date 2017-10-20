ed24cfe347188b00fd91a628233b70d76f40f479
songbinliu <songbin.liu@turbonomic.com>
2017-10-20 09:18:44 -0400

    add cmd options

--- scripts/container.run.sh
+++ scripts/container.run.sh
@@ -1,9 +1,48 @@
-#!/bin/sh
+#!/bin/sh 
 
-options="--v=2 --logtostderr"
-options="$options --modeldir=/tmp/model-data/inception/"
-options="$options --imgdir=/tmp/imgs/" 
+serverbin=/bin/inceptions
 
-bin=/bin/inceptions
-chmod +x $bin
-$bin $options
+logv=3
+alsologtostderr=true
+modeldir="/tmp/model-data/inception"
+imgdir="/tmp/imgs/"
+
+
+for i in "$@"
+do
+case $i in
+    -v=*|--v=*)
+    logv="${i#*=}"
+    shift # past argument=value
+    ;;
+    --alsologtostderr*)
+    alsologtostderr="${i#*=}"
+    shift # past argument=value
+    ;;
+    --modeldir=*)
+    modeldir="${i#*=}"
+    shift # past argument=value
+    ;;
+    --imgdir=*)
+    imgdir="${i#*=}"
+    shift # past argument=value
+    ;;
+    -h|--help)
+    $serverbin --help
+    exit 0
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+
+opts="--alsologtostderr=$alsologtostderr"
+opts="$opts --v=$logv"
+opts="$opts --modeldir=$modeldir"
+opts="$opts --imgdir=$imgdir"
+
+cmd="$serverbin $opts"
+echo "$cmd"
+
+$cmd
